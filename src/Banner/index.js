88c1f117598d0c65bf9035002a26a46a41b0af43
Brandon <brandonwkipp@gmail.com>
2019-06-26 17:25:44 -0500

    feat: Added Banner

---
+++ src/Banner/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import {
+  Jumbotron,
+} from 'reactstrap';
+
+const Banner = () => (
+  <Fragment>
+    <Jumbotron className="w-100 text-center">
+      <h1>Mud-Pulse Telemetry!</h1>
+    </Jumbotron>
+  </Fragment>
+);
+
+export default Banner;
