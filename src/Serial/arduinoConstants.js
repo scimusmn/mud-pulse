3de525e3d15549d39d50618d95f0dabbba7c9598
Brandon <brandonwkipp@gmail.com>
2019-09-02 16:01:36 -0500

    refactor: Removed unecessary serial code

--- src/Serial/arduinoConstants.js
+++
@@ -1,3 +0,0 @@
-export const ARDUINO_READY = { message: 'arduino-ready', value: 1 };
-export const WAKE_ARDUINO = { message: 'wake-arduino', value: 1 };
-export const BUTTON_PRESSED = { message: 'button-press', value: 1 };
