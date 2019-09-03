4783cbe36617f15dd5589ef6181eb471e31fa286
Brandon <brandonwkipp@gmail.com>
2019-09-03 15:39:57 -0500

    refactor: Consolidated LCD arduino sketch with mainline sketch

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -1,21 +1,31 @@
-// MudPulse
+// Mud Pulse
+
+// To be used with any 4D Systems Visi-Genie Gauges
+// Reset operation is inverted, so that a 4D systems adaptor sheild is not needed.
+// Connect TX on display connector to TX on Arduino
+// Connect RX on display connenctor to RX on Arduino
+// DO NOT power display using built-in 5V regulator on Arduino
+// Refer to Visi-Genie Gauge documentation for object names
 
 #include "arduino-base/Libraries/AnalogInput.h"
 #include "arduino-base/Libraries/Button.h"
-#include "arduino-base/Libraries/SerialManager.h"
 #include "arduino-base/Libraries/Timer.h"
+#include "arduino-base/Libraries/SerialManager.h"
+
+// Use library manager in the Arduino IDE to add this library
+#include <genieArduino.h>
+
+#define analogInput1Pin A0
+#define resetLine 4
+#define button1Pin 2
 
 SerialManager serialManager;
-
-long baudRate = 115200;
-
+long baudRate = 9600;
 AnalogInput analogInput1;
+int traceValue;
 Button button1;
+Genie genie;
 Timer timer1;
-
-// Pin assignments
-#define analogInput1Pin A0
-#define button1Pin 2
 
 int currentAnalogInput1Value = 0;
 int pulseCount = 0;
@@ -30,47 +40,82 @@ void setup() {
   boolean arduinoJsonDebug = false;
 
   // Ensure Serial Port is open and ready to communicate
-  serialManager.setup(baudRate, [](char* message, int value) {
+  serialManager.setup(115200, [](char* message, int value) {
     onParse(message, value);
   }, arduinoJsonDebug);
 
-  // ANALOG INPUTS
+  // LCD display is on hardware serial "1"
+  Serial1.begin(baudRate);
+  genie.Begin(Serial1);
 
+  // Set D4 on Arduino to Output
+  pinMode(resetLine, OUTPUT);
+
+  // Reset the Display via D4
+  digitalWrite(resetLine, 0);
+  delay(100);
+
+  // unReset the Display via D4
+  digitalWrite(resetLine, 1);
+  delay(4000);
+
+  // ANALOG INPUTS
   // We need to do averaging or we'll crash the app
   boolean enableAverager = true;
   // Sampling Rate shoud be high to throw out unecessary data, but low enough to not impact performance
-  int samplingRate = 200;
+  int samplingRate = 5;
   // We don't want use LowPass because that will make the graph not as responsive
   boolean enableLowPass = false;
-
   analogInput1.setup(analogInput1Pin, enableAverager, samplingRate, enableLowPass, [](int analogInputValue) {
+
     currentAnalogInput1Value = analogInputValue;
-    serialManager.sendJsonMessage("pressure-reading", analogInputValue);
+
+    // Map values for scope plot
+    traceValue = map(currentAnalogInput1Value, 0, 1023, 0, 100);
   });
 
   // DIGITAL INPUTS
-
-  // Parameter 1: pin location
-  // Parameter 2: callback
-
   button1.setup(button1Pin, [](int state) {
-    if (state) {
+    if (!state) {
       if (timer1.isRunning() == false) {
+
+        // Tell application to start listening to data
+        serialManager.sendJsonMessage("button-press", 1);
         pulseCount = 0;
+
+        // Get ready caption
+        genie.WriteObject(GENIE_OBJ_FORM, 1, 1);
+        delay(1000);
+
+        // Get set...caption
+        genie.WriteObject(GENIE_OBJ_FORM, 2, 1);
+        delay(1000);
+
+        // Go! caption
+        genie.WriteObject(GENIE_OBJ_FORM, 3, 1);
+        delay(500);
+
+        // Clear previous data and write zeros width of scope display
+        for (int i = 0; i < 75; i++) {
+          genie.WriteObject(GENIE_OBJ_SCOPE, 0x00, 0);
+        }
+
+        // Show live scope
+        genie.WriteObject(GENIE_OBJ_FORM, 0, 1);
+
         timer1.start();
       }
-
-      serialManager.sendJsonMessage("button-press", 1);
     }
   });
 
+  // TIMER
   timer1.setup([](boolean running, boolean ended, unsigned long timeElapsed) {
     if (running == true) {
+      serialManager.sendJsonMessage("pressure-reading", currentAnalogInput1Value);
       if (currentAnalogInput1Value > 250 && newread == true) {
         newread = false;
         pulseCount++;
       }
-
       if (currentAnalogInput1Value < 200) {
         newread = true;
       }
@@ -78,22 +123,34 @@ void setup() {
     else if (ended == true) {
       serialManager.sendJsonMessage("time-up", 1);
       serialManager.sendJsonMessage("material", pulseCount);
+      delay(1000);
+      genie.WriteObject(GENIE_OBJ_FORM, 4, 1); //message sent to computer caption
+      for (int i = 0; i < 75; i++) { //clear previous data
+        genie.WriteObject(GENIE_OBJ_SCOPE, 0, 0); //write zeros width of scope display
+      }
+      delay(2000);
+      genie.WriteObject(GENIE_OBJ_FORM, 0, 1); //show live scope
     }
   }, timerDuration);
 }
 
 void loop() {
   analogInput1.idle();
+
+  // Write the mapped values
+  genie.WriteObject(GENIE_OBJ_SCOPE, 0x00, traceValue);
+
   button1.idle();
   serialManager.idle();
   timer1.idle();
 }
 
+
 void onParse(char* message, int value) {
   if (strcmp(message, "pressure-reading") == 0 && value == 1) {
     serialManager.sendJsonMessage(message, analogInput1.readValue());
   }
-  else if (strcmp(message, "wake-arduino") == 0 && value == 1) {
+  if (strcmp(message, "wake-arduino") == 0 && value == 1) {
     serialManager.sendJsonMessage("arduino-ready", 1);
   }
   else {
