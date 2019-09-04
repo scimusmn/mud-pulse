69ce8a83bcff208ff4eea9f1cb8b900c25d5c962
= <brandonwkipp@gmail.com>
2019-09-04 14:13:33 -0500

    refactor: Added instruction state/logic

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import propTypes from 'prop-types';
 import './index.css';
+import { strata1, strata2, strata3 } from './messages';
 import withSerialCommunication from '../Arduino/arduino-base/ReactSerial/SerialHOC';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      instruction: strata1(),
       rock: '',
       status: 'waiting',
     };
@@ -22,6 +24,33 @@ class Dashboard extends Component {
   }
 
   onSerialData(data) {
+    if (data.message === 'material') {
+      let instruction = '';
+      switch (data.value) {
+        case 2:
+          instruction = strata2();
+          break;
+        case 3:
+          instruction = strata3();
+          break;
+        case 4:
+          instruction = strata3();
+          break;
+        case 5:
+          // ???
+          instruction = strata1();
+          break;
+        default:
+          // ???
+          instruction = strata1();
+          break;
+      }
+
+      this.setState({
+        instruction,
+      });
+    }
+
     if (data.message === 'button-press') {
       this.setState({
         status: 'graphing',
@@ -67,6 +96,11 @@ class Dashboard extends Component {
 
   render() {
     /* eslint arrow-body-style: 0 */
+    const instructionText = () => {
+      const { instruction } = this.state;
+      return instruction;
+    };
+
     const rockElement = () => {
       const { rock, status } = this.state;
       if (status === 'graphing') {
@@ -108,6 +142,7 @@ class Dashboard extends Component {
     return (
       <Fragment>
         <div id="dashboard" className="px-2 py-2">
+          <h2>{instructionText()}</h2>
           <h3>{dashboardStatus()}</h3>
           <h4>{rockElement()}</h4>
         </div>
