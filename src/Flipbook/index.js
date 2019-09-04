3d7757f9b815ccaf089fae2b7e48276e21e2877f
= <brandonwkipp@gmail.com>
2019-09-04 14:12:03 -0500

    refactor: Removed instruction state/logic

--- src/Flipbook/index.js
+++ src/Flipbook/index.js
@@ -1,17 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import propTypes from 'prop-types';
 import withSerialCommunication from '../Arduino/arduino-base/ReactSerial/SerialHOC';
-import bkg1 from './images/Illustration_GTS-01.jpg';
-import bkg2 from './images/Illustration_GTS-02.jpg';
-import bkg3 from './images/Illustration_GTS-04.jpg';
-import { strata1, strata2, strata3 } from './messages';
+import './index.css';
 
 class Flipbook extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: bkg1,
-      text: strata1(),
+      strata: 'strata1',
     };
 
     this.onSerialData = this.onSerialData.bind(this);
@@ -25,56 +21,37 @@ class Flipbook extends Component {
 
   onSerialData(data) {
     if (data.message === 'material') {
+      let strata = '';
       switch (data.value) {
         case 2:
-          this.setState({
-            image: bkg2,
-            text: strata2(),
-          });
+          strata = 'strata1';
           break;
         case 3:
-          this.setState({
-            image: bkg3,
-            text: strata3(),
-          });
+          strata = 'strata2';
           break;
         case 4:
-          this.setState({
-            image: bkg3,
-            text: strata3(),
-          });
+          strata = 'strata3';
           break;
         case 5:
-          // ???
-          this.setState({
-            image: bkg1,
-            text: strata1(),
-          });
+          strata = 'strata1';
           break;
         default:
-          // ???
-          this.setState({
-            image: bkg1,
-            text: strata1(),
-          });
+          strata = 'strata1';
           break;
       }
+
+      this.setState({
+        strata,
+      });
     }
   }
 
   render() {
-    const { image, text } = this.state;
+    const { strata } = this.state;
 
     return (
       <Fragment>
-        <div className="px-2 py-2">
-          <div className="w-100">
-            <img src={image} className="d-inline-block w-75" alt="lol" />
-            <div className="d-inline-block w-25">
-              <div className="px-3 text-light">{text}</div>
-            </div>
-          </div>
-        </div>
+        <div id="flipbook" className={strata} />
       </Fragment>
     );
   }
