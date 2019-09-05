18d91189430c6e50116cb08f21126c8746fa3e3a
Brandon <brandonwkipp@gmail.com>
2019-09-05 14:14:34 -0500

    refactor: Updated structure and instructions

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -9,12 +9,10 @@ class Dashboard extends Component {
     super(props);
     this.state = {
       instruction: strata1(),
-      rock: '',
       status: 'waiting',
     };
 
     this.onSerialData = this.onSerialData.bind(this);
-    this.updatePulse = this.updatePulse.bind(this);
   }
 
   componentDidMount() {
@@ -57,29 +55,6 @@ class Dashboard extends Component {
       });
     }
 
-    if (data.message === 'material') {
-      let rock = '';
-      switch (data.value) {
-        case 2:
-          rock = 'You\'ve hit Limestone!';
-          break;
-        case 3:
-          rock = 'You\'ve hit Dolomite!';
-          break;
-        case 4:
-          rock = 'You\'ve hit Shale!';
-          break;
-        case 5:
-          rock = 'You\'ve hit Sandstone!';
-          break;
-        default:
-          rock = 'Invalid pulse information. Please try again!';
-          break;
-      }
-
-      this.updatePulse(rock);
-    }
-
     // Ending sampling
     if (data.message === 'time-up') {
       this.setState({
@@ -88,34 +63,11 @@ class Dashboard extends Component {
     }
   }
 
-  updatePulse(rock) {
-    this.setState({
-      rock,
-    });
-  }
-
   render() {
     /* eslint arrow-body-style: 0 */
     const instructionText = () => {
       const { instruction } = this.state;
       return instruction;
-    };
-
-    const rockElement = () => {
-      const { rock, status } = this.state;
-      if (status === 'graphing') {
-        return (
-          'Sensing...'
-        );
-      }
-
-      if (rock !== '') {
-        return (
-          rock
-        );
-      }
-
-      return ('Nothing yet');
     };
 
     const dashboardStatus = () => {
@@ -139,12 +91,17 @@ class Dashboard extends Component {
       );
     };
 
+    console.log(dashboardStatus());
+
     return (
       <Fragment>
-        <div id="dashboard" className="px-2 py-2">
-          <h2>{instructionText()}</h2>
-          <h3>{dashboardStatus()}</h3>
-          <h4>{rockElement()}</h4>
+        <div id="dashboard" className="h-100 px-2">
+          <h2 className="text-center">
+            <span role="img" aria-label="exclamation">❗</span>
+            <u>Objective</u>
+            <span role="img" aria-label="exclamation">❗</span>
+          </h2>
+          {instructionText()}
         </div>
       </Fragment>
     );
--- src/Dashboard/messages.js
+++ src/Dashboard/messages.js
@@ -3,8 +3,8 @@ import React, { Fragment } from 'react';
 export const strata1 = () => (
   <Fragment>
     <h4 className="mb-5">You are in the first strata. The drill operator has requested data.</h4>
-    <h4 className="mb-5">Press the start button and squeeze the tube 2 times to send your message.</h4>
-    <h4 className="mb-5">Watch the message screen to see which strata you’ve reached.</h4>
+    <h4 className="mb-5">Press the start button and squeeze the tube 5 times to send your message.</h4>
+    {/* <h4>Watch the message screen to see which strata you’ve reached.</h4> */}
   </Fragment>
 );
 
@@ -12,7 +12,7 @@ export const strata2 = () => (
   <Fragment>
     <h4 className="mb-5">You have reached the second strata.</h4>
     <h4 className="mb-5">Press the start button and squeeze the tube 3 times to send your message.</h4>
-    <h4 className="mb-5">Watch the message screen to see which strata you’ve reached.</h4>
+    {/* <h4>Watch the message screen to see which strata you’ve reached.</h4> */}
   </Fragment>
 );
 
@@ -20,7 +20,7 @@ export const strata3 = () => (
   <Fragment>
     <h4 className="mb-5">You have reached the third strata.</h4>
     <h4 className="mb-5">Press the start button and squeeze the tube 4 times to send your message.</h4>
-    <h4 className="mb-5">Watch the message screen to see which strata you’ve reached.</h4>
+    {/* <h4>Watch the message screen to see which strata you’ve reached.</h4> */}
   </Fragment>
 );
 
@@ -28,6 +28,6 @@ export const strata4 = () => (
   <Fragment>
     <h4 className="mb-5">Congratulations!</h4>
     <h4 className="mb-5">You’ve reached the shale strata.</h4>
-    <h4 className="mb-5">Now the oil can be extracted.</h4>
+    <h4>Now the oil can be extracted.</h4>
   </Fragment>
 );
