a16a855984d2e2462f06b88235d7a35b13a6a68a
Brandon <brandonwkipp@gmail.com>
2019-09-13 15:52:59 -0500

    refactor: Fixed strata update

--- src/Strata/index.js
+++ src/Strata/index.js
@@ -1,63 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import propTypes from 'prop-types';
 import './index.css';
-import withSerialCommunication from '../Arduino/arduino-base/ReactSerial/SerialHOC';
 
 class Strata extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      strata: 'Unknown',
     };
-
-    this.onSerialData = this.onSerialData.bind(this);
-    this.updateDisplay = this.updateDisplay.bind(this);
   }
 
   componentDidMount() {
-    const { setOnDataCallback } = this.props;
-    setOnDataCallback(this.onSerialData);
     document.addEventListener('keydown', this.handleReset);
   }
 
-  onSerialData(data) {
-    if (data.message === 'material') {
-      let strata = '';
-      switch (data.value) {
-        case 2:
-          strata = 'Limestone';
-          break;
-        case 3:
-          strata = 'Dolomite';
-          break;
-        case 4:
-          strata = 'Shale';
-          break;
-        case 5:
-          strata = 'Sandstone';
-          break;
-        default:
-          strata = 'Unknown';
-          break;
-      }
-
-      this.updateDisplay(strata);
-    }
-  }
-
-  updateDisplay(strata) {
-    this.setState({
-      strata,
-    });
-  }
-
   render() {
-    const { strata } = this.state;
+    const { strata } = this.props;
 
     return (
       <Fragment>
         <div id="strataDisplay" className="px-2 text-center">
-          <h2><u>Strata Location</u></h2>
+          <h2>
+            <u>Last Verified Strata</u>
+          </h2>
           {strata}
         </div>
       </Fragment>
@@ -66,8 +30,7 @@ class Strata extends Component {
 }
 
 Strata.propTypes = {
-  setOnDataCallback: propTypes.func.isRequired,
+  strata: propTypes.string.isRequired,
 };
 
-const StrataWithSerialCommunication = withSerialCommunication(Strata);
-export default StrataWithSerialCommunication;
+export default Strata;
