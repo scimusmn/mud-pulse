e6efdb28a93ee44e8cf9fb9d1ae5de5294b84872
Brandon <brandonwkipp@gmail.com>
2020-02-24 16:56:40 -0600

    refactor: Updated arduino sketch to use new arduino-base libraries, fixed some missing logic for graphing

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -7,23 +7,23 @@
 // DO NOT power display using built-in 5V regulator on Arduino
 // Refer to Visi-Genie Gauge documentation for object names
 
-#include "arduino-base/Libraries/Averager.h"/
-#include "arduino-base/Libraries/SerialManager.h"
+#include "arduino-base/Libraries/Averager.h"
+#include "arduino-base/Libraries/SerialController.hpp"
 
 // Use library manager in the Arduino IDE to add this library
-#include <genieArduino.h>
+// #include <genieArduino.h>
 
 #define analogInput1Pin A0
 #define resetLine 4
 
 // Stele communication
-SerialManager serialManager;
+SerialController serialController;
 long steleBaudRate = 115200;
 
 // Genie communication
 long genieBaudRate = 9600;
 
-Genie genie;
+// Genie genie;
 Averager averager;
 
 int allowGraphing = 0;  // flag that sends pres data to stele
@@ -34,6 +34,7 @@ int pulseCount = 0;
 int millisBetweenSample = 5; //millis between taking analog read
 int timerDuration = 5000;  // time in millis to send pressure data
 bool rising = false; // used for pulse detection
+
 // Set threshold band
 int hysteresis = 20;
 unsigned long graphStartMillis;
@@ -43,17 +44,14 @@ unsigned long lastSampleMillis;
 void setup() {
   averager.setup(10, false);
 
-  // Enables/disables debug messaging from ArduinoJson
-  boolean arduinoJsonDebug = false;
-
   // Ensure Serial Port is open and ready to communicate
-  serialManager.setup(steleBaudRate, [](char* message, int value) {
+  serialController.setup(steleBaudRate, [](char* message, char* value) {
     onParse(message, value);
-  }, arduinoJsonDebug);
+  });
 
-  //   LCD display is on hardware Serial1
-  Serial1.begin(genieBaudRate);
-  genie.Begin(Serial1);
+  // LCD display is on hardware Serial1
+  //Serial1.begin(genieBaudRate);
+  //genie.Begin(Serial1);
 
   pinMode(analogInput1Pin, INPUT);
 
@@ -67,66 +65,65 @@ void setup() {
   // unReset the Display via D4
   digitalWrite(resetLine, 1);
   delay(4000);
-
 }
 
 void loop() {
   currentMillis = millis();
 
   // end graphing after timerDuration
-  if ((currentMillis - graphStartMillis > timerDuration) && (allowGraphing)) {
+  if ((currentMillis - graphStartMillis > timerDuration) && allowGraphing) {
     allowGraphing = 0;
-    serialManager.sendJsonMessage("time-up", 1);
-    serialManager.sendJsonMessage("material", pulseCount);
+    serialController.sendMessage("time-up", 1);
+    serialController.sendMessage("material", pulseCount);
   }
 
   // take a sample every 'millisBetweenSample'
   if (currentMillis - lastSampleMillis > millisBetweenSample) {
     averagePressure = analogRead(analogInput1Pin);
 
-    //scale pressure data for 4d display
+    // scale pressure data for 4d display
     traceValue = map(averagePressure, 0, 1023, 0, 100);
 
     // if graphing, monitor for pulses and send pressure data to Stele
     if (allowGraphing) {
       if (averagePressure >= lastAvgPres + hysteresis) {
         rising = true;
-
-      //if it was rising but is now falling, count a pulse.
       } else if ((rising) && (averagePressure <= lastAvgPres - hysteresis)) {
+        // if it was rising but is now falling, count a pulse.
         rising = false;
         pulseCount++;
       }
+
       // send a pressure reading to stele
-      serialManager.sendJsonMessage("pressure-reading", averagePressure);
+      serialController.sendMessage("pressure-reading", averagePressure);
     }
 
-
     // Write the mapped values to small screen
-    genie.WriteObject(GENIE_OBJ_SCOPE, 0x00, traceValue);
+    // genie.WriteObject(GENIE_OBJ_SCOPE, 0x00, traceValue);
 
-    //store info for last reading
+    // store info for last reading
     lastAvgPres = averagePressure;
     lastSampleMillis = currentMillis;
   }
 
-  serialManager.idle();
+  serialController.update();
 }
 
-void onParse(char* message, int value) {
-
-  //listen for request of 5 sec of pressure data
-  if (strcmp(message, "allow-graphing") == 0) { // Tell arduino to send 5 sec of pres data
+void onParse(char* message, char* value) {
+  // listen for request of 5 sec of pressure data
+  if (strcmp(message, "allow-graphing") == 0 && atoi(value) == 1) {
+    // Tell arduino to send 5 sec of pres data
     allowGraphing = value;
+
     if (allowGraphing) {
       pulseCount = 0;
       graphStartMillis = millis();
     }
   }
-  else if (strcmp(message, "wake-arduino") == 0 && value == 1) {
-    serialManager.sendJsonMessage("arduino-ready", 1);
+  else if (strcmp(message, "wake-arduino") == 0 && atoi(value) == 1) {
+    serialController.sendMessage("arduino-ready", 1);
   }
   else {
-    serialManager.sendJsonMessage("unknown-command", 1);
+    serialController.sendMessage("unknown-command", 1);
   }
 }
